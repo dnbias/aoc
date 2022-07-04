240e03b0150eeb413e313e103b5f6e46ba807413
dnbias <daniel.biasiotto@edu.unito.it>
2022-07-05 01:44:43 +0200

    feat: big crazy filters

--- 2021/rust/day3/main.rs
+++ 2021/rust/day3/main.rs
@@ -1,8 +1,22 @@
+#![allow(dead_code)]
 mod diagnostic;
 use diagnostic::calculate_diagnostic;
+use diagnostic::ratings;
 
 fn main() {
+    // power_consumption();
+    life_support_rating();
+}
+
+fn power_consumption() {
     let (gamma,epsilon) = calculate_diagnostic("input").unwrap();
+    let gamma = gamma as u64;
+    let epsilon = epsilon as u64;
     let power_consumption = gamma * epsilon;
     println!("Power Consumption: {}", power_consumption);
+}
+
+fn life_support_rating() {
+    let (ox,co) = ratings("input").unwrap();
+    println!("Life Support Rating: {}", ox*co);
 }
